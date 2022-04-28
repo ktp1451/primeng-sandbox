4380969ac79626cda7b6f172acb387c1e6fb4b50
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2022-04-28 14:56:13 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primeng-sandbox';
+  title!: string; /**added (!) definite assignment assertion to property**/
+
+  upper() {
+    this.title = this.title.toUpperCase();
+  }
 }
